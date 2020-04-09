e8288dd870cd6fcbab1723701c7642dce204e176
Vasyl Kosiv <vasylkosiv@gmail.com>
2020-04-09 19:11:03 +0300

    Remove binfile

---
+++ Task107.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	filename := flag.String("f", "", "Take argument from file")
+	flag.Parse()
+	var k int = 0
+
+	if len(*filename) != 0 {
+		k = findk(inputArg(inputFile(filename)))
+		fmt.Println("k =", k)
+
+	} else if len(os.Args) > 1 {
+		var mSt = os.Args[1]
+		k = findk(inputArg(mSt))
+		fmt.Println("k =", k)
+	} else {
+
+		k = findk(inputArg(inputConsole()))
+		fmt.Println("k =", k)
+
+	}
+}
+func findk(x int) int {
+	var mulFour = 1
+	var k = 0
+	for i := 0; x > mulFour; i++ {
+		mulFour *= 4
+		k = i
+	}
+	return (k)
+
+}
+
+func inputArg(x string) int {
+
+	var m, err = strconv.Atoi(x)
+	if err != nil {
+		panic(err)
+
+	}
+
+	return m
+}
+
+func inputFile(filename *string) string {
+
+	file, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	var fileData []string
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fileData = append(fileData, scanner.Text())
+	}
+	var mFile = strings.Join(fileData, "")
+	return mFile
+}
+
+func inputConsole() string {
+	var consoleData string
+
+	fmt.Println("Enter a natural number grater than 1:")
+	fmt.Fscan(os.Stdin, &consoleData)
+
+	return consoleData
+
+}
---
+++ Task107_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_findk(t *testing.T) {
+
+	tests := []struct {
+		name string
+		args int
+		want int
+	}{
+
+		{
+			name: "CorrectInput",
+			args: 567,
+			want: 4,
+		},
+		{
+			name: "ZeroInput",
+			args: 0,
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findk(tt.args); got != tt.want {
+				t.Errorf("findk() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_inputArg(t *testing.T) {
+
+	tests := []struct {
+		name string
+		args string
+		want int
+	}{
+		{
+			name: "CorrectInput",
+			args: "567",
+			want: 567,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inputArg(tt.args); got != tt.want {
+				t.Errorf("inputArg() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_inputConsole(t *testing.T) {
+	tests := []struct {
+		name string
+
+		want string
+	}{
+		{
+			name: "CorrectInput",
+
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inputConsole(); got != tt.want {
+				t.Errorf("inputConsole() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
